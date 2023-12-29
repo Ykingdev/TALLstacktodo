2ee84dc903e22b2c5752eda6c5aa015764c250e5
Y-Shinden King <86405969+Ykingdev@users.noreply.github.com>
2023-12-29 14:06:38 +0100

    Update list-notes.blade.php

--- resources/views/livewire/board/notes/list-notes.blade.php
+++ resources/views/livewire/board/notes/list-notes.blade.php
@@ -1,29 +1,26 @@
-<div x-data="{ draggingNoteId: null, droppedStatus: null, mobileSelectNoteId: null }" class="flex flex-col md:flex-row w-full justify-center">
+<div x-data="{ draggingNoteId: null, droppedStatus: null }" class="flex flex-col md:flex-row w-[80vw] justify-center">
     {{-- To Do Column --}}
-    <div class="w-full md:w-1/3 px-2 md:px-8 py-4" @drop.prevent="droppedStatus = 'ToDo'" @dragover.prevent>
+    <div class="max-w-7xl sm:px-6 lg:px-8 py-4" @drop.prevent="droppedStatus = 'ToDo'" @dragover.prevent>
         <div class="flex flex-row mb-4 justify-between items-center bg-yellow-200 rounded-md p-2 border-2">
-            <h1 class="text-lg md:text-2xl ml-3 font-bold text-gray-900">To Do</h1>
+            <h1 class="text-2xl ml-3  font-bold text-gray-900 ">To Do</h1>
             <a href="{{ route('notes.create') }}">
-                <x-heroicon-o-plus-circle class="w-6 h-6 md:w-8 md:h-8 text-green-500 hover:text-green-300 hover:rotate-90 hover:duration-500 cursor-pointer" />
+            <x-heroicon-o-plus-circle class="w-8 h-8 text-green-500 hover:text-green-300 hover:rotate-90 hover:duration-500 cursor-pointer" />
             </a>
+
         </div>
-        <div class="bg-white rounded-lg w-full overflow-auto">
+        <div class="bg-white sm:rounded-lg h-[70vh] w-[20vw] overflow-scroll gap-2">
             @foreach($notes as $note)
-                <div
-                    draggable="true"
-                    @dragstart="draggingNoteId = '{{ $note->id }}'"
-                    @dragend="$wire.call('moveNote', draggingNoteId, droppedStatus)"
-                    @click.away="mobileSelectNoteId = null"
-                    @click="mobileSelectNoteId = '{{ $note->id }}'"
-                    class="bg-white p-4 md:p-6 mb-4 border-2 rounded-md">
+                <div draggable="true" @dragstart="draggingNoteId = '{{ $note->id }}'" @dragend="$wire.call('moveNote', draggingNoteId, droppedStatus)" class="bg-white hover:scale-105 duration-150 hover:duration-150 border-gray-50 shadow-sm rounded-md p-6 mb-4 border-2">
                     <div class="flex flex-row justify-between">
                         <div class="flex flex-col">
-                            <h1 class="text-md md:text-lg font-bold text-gray-900">{{ $note->title }}</h1>
-                            <h2 class="text-xs md:text-sm text-gray-500 ml-2">{{ $note->created_at->diffForHumans() }}</h2>
-                            <p class="text-xs md:text-sm text-gray-500">{{ $note->body }}</p>
-                            <span class="text-xs md:text-sm text-gray-500">{{ $note->due_date->diffForHumans() }}</span>
+                            <div class="flex flex-row items-center justify-between w-[20rem]">
+                                <h1 class="text-lg font-bold text-gray-900">{{ $note->title }}</h1>
+                                <h2 class="text-sm text-gray-500 ml-2">{{ $note->created_at->diffForHumans() }}</h2>
+                            </div>
+                            <p class="text-sm text-gray-500">{{ $note->body }}</p>
+                            <span class="text-sm text-gray-500">{{ $note->due_date->diffForHumans() }}</span>
                             <div class="flex flex-row justify-end gap-5">
-                                <a href="{{ route('deleteNote', $note->id) }}" class="text-xs md:text-sm text-red-500">Delete</a>
+                                <a href="{{ route('deleteNote', $note->id) }}" class="text-sm text-red-500">Delete</a>
                             </div>
                         </div>
                     </div>
@@ -33,25 +30,22 @@
     </div>
 
     {{-- In Progress Column --}}
-    <div class="w-full md:w-1/3 px-2 md:px-8 py-4" @drop.prevent="droppedStatus = 'InProgress'" @dragover.prevent>
-        <h1 class="text-lg md:text-2xl mb-4 font-bold text-gray-900 pl-3 bg-blue-200 p-2 rounded-md border-2">In progress...</h1>
-        <div class="bg-white rounded-lg w-full overflow-auto">
+    <div class="bg-white h-full overflow-scroll gap-2 px-8 border-x-2 border-gray-200  py-4" @drop.prevent="droppedStatus = 'InProgress'" @dragover.prevent>
+        <h1 class="text-2xl mb-4 font-bold text-gray-900 pl-3 bg-blue-200 p-2 rounded-md w-full border-2 ">In progress...</h1>
+        <div class="bg-white sm:rounded-lg h-[70vh] w-[20vw] overflow-scroll gap-2">
             @foreach($inProgressNotes as $inProgressNote)
-                <div
-                    draggable="true"
-                    @dragstart="draggingNoteId = '{{ $inProgressNote->id }}'"
-                    @dragend="$wire.call('moveNote', draggingNoteId, droppedStatus)"
-                    @click.away="mobileSelectNoteId = null"
-                    @click="mobileSelectNoteId = '{{ $inProgressNote->id }}'"
-                    class="bg-white p-4 md:p-6 mb-4 border-2 rounded-md">
+                <div draggable="true" @dragstart="draggingNoteId = '{{ $inProgressNote->id }}'" @dragend="$wire.call('moveNote', draggingNoteId, droppedStatus)" class="bg-white hover:scale-105 duration-150 hover:duration-150 border-gray-50 shadow-sm rounded-md p-6 mb-4 border-2">
                     <div class="flex flex-row justify-between">
                         <div class="flex flex-col">
-                            <h1 class="text-md md:text-lg font-bold text-gray-900">{{ $inProgressNote->title }}</h1>
-                            <h2 class="text-xs md:text-sm text-gray-500 ml-2">{{ $inProgressNote->created_at->diffForHumans() }}</h2>
-                            <p class="text-xs md:text-sm text-gray-500">{{ $inProgressNote->body }}</p>
-                            <span class="text-xs md:text-sm text-gray-500">{{ $inProgressNote->due_date->diffForHumans() }}</span>
+                            <div class="flex flex-row items-center justify-between w-[20rem]">
+                                <h1 class="text-lg font-bold text-gray-900">{{ $inProgressNote->title }}</h1>
+                                <h2 class="text-sm text-gray-500 ml-2">{{ $inProgressNote->created_at->diffForHumans() }}</h2>
+                            </div>
+                            <p class="text-sm text-gray-500">{{ $inProgressNote->body }}</p>
+                            <span class="text-sm text-gray-500">{{ $inProgressNote->due_date->diffForHumans() }}</span>
                             <div class="flex flex-row justify-end gap-5">
-                                <a href="{{ route('deleteNote', $inProgressNote->id) }}" class="text-xs md:text-sm text-red-500">Delete</a>
+                                <a href="{{ route('deleteNote', $inProgressNote->id) }}" class="text-sm text-red-500">Delete</a>
+
                             </div>
                         </div>
                     </div>
@@ -61,25 +55,21 @@
     </div>
 
     {{-- Complete Column --}}
-    <div class="w-full md:w-1/3 px-2 md:px-8 py-4" @drop.prevent="droppedStatus = 'Finished'" @dragover.prevent>
-        <h1 class="text-lg md:text-2xl pl-3 mb-4 font-bold text-gray-900 bg-green-200 p-2 rounded-md border-2">Complete</h1>
-        <div class="bg-white rounded-lg w-full overflow-auto">
+    <div class="max-w-7xl sm:px-6 lg:px-8 py-4" @drop.prevent="droppedStatus = 'Finished'" @dragover.prevent>
+        <h1 class="text-2xl pl-3 mb-4 font-bold text-gray-900 bg-green-200 p-2 rounded-md border-2">Complete</h1>
+        <div class="bg-white sm:rounded-lg h-[70vh] w-[20vw] overflow-scroll gap-2">
             @foreach($CompletedNotes as $CompletedNote)
-                <div
-                    draggable="true"
-                    @dragstart="draggingNoteId = '{{ $CompletedNote->id }}'"
-                    @dragend="$wire.call('moveNote', draggingNoteId, droppedStatus)"
-                    @click.away="mobileSelectNoteId = null"
-                    @click="mobileSelectNoteId = '{{ $CompletedNote->id }}'"
-                    class="bg-white p-4 md:p-6 mb-4 border-2 rounded-md">
+                <div draggable="true" @dragstart="draggingNoteId = '{{ $CompletedNote->id }}'" @dragend="$wire.call('moveNote', draggingNoteId, droppedStatus)" class="bg-white hover:scale-105 duration-150 hover:duration-150 border-gray-50 shadow-sm rounded-md p-6 mb-4 border-2">
                     <div class="flex flex-row justify-between">
                         <div class="flex flex-col">
-                            <h1 class="text-md md:text-lg font-bold text-gray-900">{{ $CompletedNote->title }}</h1>
-                            <h2 class="text-xs md:text-sm text-gray-500 ml-2">{{ $CompletedNote->created_at->diffForHumans() }}</h2>
-                            <p class="text-xs md:text-sm text-gray-500">{{ $CompletedNote->body }}</p>
-                            <span class="text-xs md:text-sm text-gray-500">{{ $CompletedNote->due_date->diffForHumans() }}</span>
+                            <div class="flex flex-row items-center justify-between w-[20rem]">
+                                <h1 class="text-lg font-bold text-gray-900">{{ $CompletedNote->title }}</h1>
+                                <h2 class="text-sm text-gray-500 ml-2">{{ $CompletedNote->created_at->diffForHumans() }}</h2>
+                            </div>
+                            <p class="text-sm text-gray-500">{{ $CompletedNote->body }}</p>
+                            <span class="text-sm text-gray-500">{{ $CompletedNote->due_date->diffForHumans() }}</span>
                             <div class="flex flex-row justify-end gap-5">
-                                <a href="{{ route('deleteNote', $CompletedNote->id) }}" class="text-xs md:text-sm text-red-500">Delete</a>
+                                <a href="{{ route('deleteNote', $CompletedNote->id) }}" class="text-sm text-red-500">Delete</a>
                             </div>
                         </div>
                     </div>
